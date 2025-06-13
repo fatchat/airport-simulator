c826dcca308a7b007d29f4426b072fc5b2c21656
Rohit Chatterjee <rohit@rootbridgeservices.in>
2025-06-13 22:39:23 +0530

    docker compose not docker-compose

--- add-component.sh
+++ add-component.sh
@@ -9,11 +9,11 @@ AIRPORT=$2
 NUMBER=$3
 
 if [ "$COMPONENT_TYPE" == "airport" ]; then
-  docker-compose run -d --name "airport-$AIRPORT" airport --airport $AIRPORT --verbose
+  docker compose run -d --name "airport-$AIRPORT" airport --airport $AIRPORT --verbose
 elif [ "$COMPONENT_TYPE" == "runway" ]; then
-  docker-compose run -d --name "runway-$AIRPORT-$NUMBER" runway --airport $AIRPORT --runway-number $NUMBER --verbose
+  docker compose run -d --name "runway-$AIRPORT-$NUMBER" runway --airport $AIRPORT --runway-number $NUMBER --verbose
 elif [ "$COMPONENT_TYPE" == "gate" ]; then
-  docker-compose run -d --name "gate-$AIRPORT-$NUMBER" gate --airport $AIRPORT --gate-number $NUMBER --verbose
+  docker compose run -d --name "gate-$AIRPORT-$NUMBER" gate --airport $AIRPORT --gate-number $NUMBER --verbose
 else
   echo "Unknown component type: $COMPONENT_TYPE"
   echo "Usage: ./add-component.sh [component-type] [airport] [number]"
